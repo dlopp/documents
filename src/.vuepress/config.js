899dbedb5372793818863de53e0aee97e28f001a
だいき <daaikii@outlook.jp>
2020-10-26 22:43:07 +0900

    create: 資料置き場の作成

---
+++ src/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: '✈︎ dlopp.',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      {
+        text: 'More',
+        items: [
+          { text: 'Twitter', link: 'https://twitter.com/wucrea_dlopp'},
+          { text: 'GitHub', link: 'https://github.com/dlopp'},
+          { text: 'HP', link: 'https://dlopp.github.io'}
+        ]
+      }
+    ],
+    sidebar: [
+      '/about/'
+    ],
+  }
+}
