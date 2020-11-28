b889f9b3797b0ef16b781f0b83ebe2c85e7fd18f
だいき <daaikii@outlook.jp>
2020-11-28 12:03:49 +0900

    fix: twitterカードのデフォルトを小さいものに変更

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -25,6 +25,7 @@ module.exports = {
       description: ($page, $site) => $page.frontmatter.description || ($page.excerpt && $page.excerpt.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")) || $site.description || "",
       title: ($page, $site) => $page.title || $site.title,
       image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'https://i.gyazo.com/96879cdaad2e8524dce6252ec6270162.jpg',
+      twitterCard: _ => 'summary',
     }
   },
 }
