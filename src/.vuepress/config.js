c2651ae16e5206846672f64cd3c06f626f1987fa
Daaiki <daaikii@outlook.jp>
2021-04-04 14:22:46 +0900

    OGP変更

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -29,7 +29,7 @@ module.exports = {
     'seo': {
       description: ($page, $site) => $page.frontmatter.description || ($page.excerpt && $page.excerpt.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")) || $site.description || "",
       title: ($page, $site) => $page.title || $site.title,
-      image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'https://i.gyazo.com/96879cdaad2e8524dce6252ec6270162.jpg',
+      image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'https://i.gyazo.com/2f4c109fe59f897e79885c0ac436c76e.png',
       twitterCard: _ => 'summary',
     },
     '@vuepress/last-updated': {
